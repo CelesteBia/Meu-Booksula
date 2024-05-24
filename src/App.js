592888f57a0d2acf186f5c9e947c8453740565c7
CelesteBia <124384519+CelesteBia@users.noreply.github.com>
2024-05-24 02:30:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Feed from './Components/Feed/Feed';
+{/*import Feed from './Components/Feed/Feed';
+import Modal from './Components/EscreverResenhas/Resenhas';
+import UserResenha from './Components/ResenhasUser/Usuario';
+import Header from './Components/Header/Header';
+import Image from "./Components/Logo/Image";
+import Forms from "./Components/Formulario/Forms";*/}
+
+function App() {
+  return (
+    <div>
+      <Feed />
+      {/*<Feed />
+      <Home />
+      <Forms/>
+      <UserResenha />
+      <Modal />
+      <Header />
+      <Image/>
+      <div className="App">
+     <br></br>
+     </div>*/}
+    </div>
+  );
+}
+
+export default App;
